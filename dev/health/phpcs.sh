cb4f2b8a7100a389c2a0c601ab541ee2b023a86f
Maikel Vitancourt <maikel-93@hotmail.com>
2021-04-14 11:25:04 -0300

    Added configurations for unit tests and grumphp tests

--- dev/health/phpcs.sh
+++ dev/health/phpcs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 echo "Running PHP Code Sniffer..."
-dev/bin/cli phpcs ./app "$@" --standard=phpcs-rulesets.xml --extensions=php
+bin/cli phpcs ./app "$@" --standard=phpcs-rulesets.xml --extensions=php
 echo ""
--- dev/health/phpmd.sh
+++ dev/health/phpmd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 echo "Running PHP Mess Detector..."
-dev/bin/cli phpmd ./app text phpmd-rulesets.xml
+bin/cli phpmd ./app text phpmd-rulesets.xml
 echo ""
